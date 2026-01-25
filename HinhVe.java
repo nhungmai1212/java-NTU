cc4a11afaed30dd9f65f54da34c6b214f005a023
Nhung Mai <mhnhung1209@gmail.com>
2026-01-25 23:15:32 +0700

    Create HinhVe.java

---
+++ HinhVe.java
@@ -0,0 +1,12 @@
+package bai1;
+
+public class HinhVe {
+
+	public static void main(String[] args) {
+		System.out.println("   *   ");
+		System.out.println("  ***  ");
+		System.out.println(" ***** ");
+		System.out.println("*******");
+	}
+
+}
