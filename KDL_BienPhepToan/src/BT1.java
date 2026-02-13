fdd7143131127a2845bfab2893b589047b5b7e8e
Nhung Mai <mhnhung1209@gmail.com>
2026-02-13 12:52:36 +0700

    Biến và toán tử

---
+++ KDL_BienPhepToan/src/BT1.java
@@ -0,0 +1,19 @@
+
+public class BT1 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 5;
+		double c = 2.5;
+		
+		int tong = a + b;
+		double hieu = b - c;
+		double thuong = a / c;
+		
+		System.out.println("Tong 2 so la: " + tong);
+		System.out.println("Hieu 2 so la: " + hieu);
+		System.out.println("Thuong 2 so la: " + thuong);
+
+	}
+
+}
