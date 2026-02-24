b7cdc019640d805e0a69303526a19df62c5bbaa8
Nhung Mai <mhnhung1209@gmail.com>
2026-02-25 00:10:27 +0700

    vd1

---
+++ SV_OOP/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		SinhVien sv1 = new SinhVien();
+		
+		sv1.ten = "Mai Hong Nhung";
+		sv1.tuoi = 20;
+		sv1.mssv = "66132668";
+		
+		sv1.Hocbai("lập trình Java.");
+
+	}
+
+}
---
+++ SV_OOP/src/SinhVien.java
@@ -0,0 +1,10 @@
+
+public class SinhVien {
+	String ten;
+	int tuoi;
+	String mssv;
+	
+	void Hocbai(String monhoc) {
+		System.out.print("Bạn " + ten + " học môn " + monhoc);
+	}
+}
