1ca09d3aa9a3550e3e1ffa990523901adcaf429d
Nhung Mai <mhnhung1209@gmail.com>
2026-04-23 08:14:33 +0700

    COMMIT lần 3

--- Thigk_cau2/src/thigk2/MaiHongNhung/Run.java
+++ Thigk_cau2/src/thigk2/MaiHongNhung/Run.java
@@ -1,10 +1,31 @@
 package thigk2.MaiHongNhung;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<SinhVien> ds = new ArrayList<>();
+		SinhVien sv1 = new SinhVien();
+		sv1.setMssv(661);
+		sv1.setHoten("Mai Hong Nhung");
+		sv1.setNamsinh(2006);
+		sv1.setLop("CNTT3");
+		System.out.println(sv1);
+		ds.add(sv1);
+		
+		SinhVien sv2 = new SinhVien(662, "Nguyen Van A", 2005, "KTE");
+		SinhVien sv3 = new SinhVien(663, "Trần Thị Hoài", 2006, "KHMT");
+		ds.add(sv2);
+		ds.add(sv3);
+		System.out.println("Danh sách sinh viên:");
+		for(SinhVien sv : ds) {
+			System.out.println(sv);
+		}
+		
+//		Scanner banphim = new Scanner(System.in);
+		
 	}
 
 }
--- Thigk_cau2/src/thigk2/MaiHongNhung/SinhVien.java
+++ Thigk_cau2/src/thigk2/MaiHongNhung/SinhVien.java
@@ -1,5 +1,60 @@
 package thigk2.MaiHongNhung;
 
 public class SinhVien {
+	private int mssv;
+	private String hoten;
+	private int namsinh;
+	private String lop;
+	
+	public SinhVien() {
+		
+	}
+	public SinhVien(int mssv, String hoten, int namsinh, String lop) {
+		this.mssv = mssv;
+		this.hoten = hoten;
+		this.namsinh = namsinh;
+		this.lop = lop;
+	}
+	public int getMssv() {
+		return mssv;
+	}
+	public void setMssv(int mssv) {
+		this.mssv = mssv;
+	}
+	public String getHoten() {
+		return hoten;
+	}
+	public void setHoten(String hoten) {
+		this.hoten = hoten;
+	}
+	public int getNamsinh() {
+		return namsinh;
+	}
+	public void setNamsinh(int namsinh) {
+		this.namsinh = namsinh;
+	}
+	public String getLop() {
+		return lop;
+	}
+	public void setLop(String lop) {
+		this.lop = lop;
+	}
+	
+	
+	@Override
+	public String toString() {
+		String chuoixuat;
+		chuoixuat =  "MSSV: " + mssv;
+		chuoixuat = chuoixuat + "\nHọ và tên: " + hoten;
+		chuoixuat = chuoixuat + "\nNăm sinh:  " + namsinh;
+		chuoixuat = chuoixuat + "\nLop: " + lop;
+		return chuoixuat;
+	}
 
+
+	
+	
+	
+	
+	
 }
