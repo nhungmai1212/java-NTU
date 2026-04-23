b005b6cd45a6734d8c7b5434d6306b569f52ae12
Nhung Mai <mhnhung1209@gmail.com>
2026-04-23 08:25:27 +0700

    COMMIT lần thứ 4

--- Thigk_cau2/src/thigk2/MaiHongNhung/Run.java
+++ Thigk_cau2/src/thigk2/MaiHongNhung/Run.java
@@ -2,6 +2,11 @@ package thigk2.MaiHongNhung;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.time.LocalDate;
+//Tao lớp tên SinhVien chứa massv, hoten, namsinh, lop
+//Tạo hàm constructor, get-set
+//Nhập dữ liệu 3 sinh viên bằng hard-code vào main - in ra
+//Thêm mới 1 sv bằng cách nhập dữ liệu từ bàn phím- in lại danh sách
 
 public class Run {
 
@@ -24,8 +29,46 @@ public class Run {
 			System.out.println(sv);
 		}
 		
-//		Scanner banphim = new Scanner(System.in);
+		Scanner banphim = new Scanner(System.in);
+		SinhVien sv4 = new SinhVien();
+		System.out.println("\nNhap MSSV: ");
+		int MSSVsv4 = banphim.nextInt();
+		banphim.nextLine();
+		sv4.setMssv(MSSVsv4);
+
+		System.out.println("\nNhap Họ và tên: ");
+		String HoTensv4 = banphim.nextLine();
+		sv4.setHoten(HoTensv4);
 		
+
+		System.out.println("\nNhap năm sinh: ");
+		int NSsv4 = banphim.nextInt();
+		sv4.setNamsinh(NSsv4);
+		banphim.nextLine();
+
+		System.out.println("\nNhap lớp: ");
+		String Lopsv4 = banphim.nextLine();
+		sv4.setLop(Lopsv4);
+		ds.add(sv4);
+		
+		System.out.println("Danh sách sinh viên sau khi thêm mới 1 sinh viên:");
+		for(SinhVien sv : ds) {
+			System.out.println(sv);
+		}
+		
+		int namHienTai = LocalDate.now().getYear();
+
+        System.out.println("\nDanh sách sinh viên > 20 tuổi:");
+
+        for (SinhVien sv : ds) {
+            int tuoi = namHienTai - sv.getNamsinh();
+
+            if (tuoi > 20) {
+                System.out.println(sv);
+                System.out.println("Tuổi: " + tuoi);
+                System.out.println("-------------------");
+            }
+        }
 	}
 
 }
